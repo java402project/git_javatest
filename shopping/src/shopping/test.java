49342896cf5cf425ef598c24d27ac306778370b9
mekeyace <mekeyace@nate.com>
2024-07-25 15:27:02 +0900

    테스트

---
+++ shopping/src/shopping/test.java
@@ -0,0 +1,13 @@
+package shopping;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "test";
+		int b = 10;
+		int c = 20;
+		String user = "hong";
+	}
+
+}
