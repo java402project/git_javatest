deb245deede00f5f7052d73147f7e5dd372901b7
airmungs <airmungs@gmail.com>
2024-07-25 16:42:08 +0900

    abc

---
+++ shopping/src/shopping/test.java
@@ -0,0 +1,13 @@
+package shopping;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "test";
+		int b = 10;
+		String user = "hong";
+		System.out.println(1234);
+	}
+
+}
