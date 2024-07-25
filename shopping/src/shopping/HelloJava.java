ca3fb69a887b4bab5c0124b91a8648e4173f1276
airmungs <airmungs@gmail.com>
2024-07-25 17:10:07 +0900

    few
    gwe

--- shopping/src/shopping/HelloJava.java
+++ shopping/src/shopping/HelloJava.java
@@ -3,7 +3,7 @@ package shopping;
 public class HelloJava {
 
 	public static void main(String[] args) {
-		System.out.println("Hello~~!!@@@@@@@@@@@#$#$!");
+		System.out.println("Hello~~!!@@@@@@@@@@@#$@@@#$!");
 
 	}
 
